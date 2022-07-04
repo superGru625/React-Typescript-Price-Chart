290592ef6ba161f1f4428995add02a658b6d148b
Mike <96395962+superGru625@users.noreply.github.com>
2022-07-04 01:24:15 -0700

    [Add] Svg icons for wallet

---
+++ src/components/Svg/index.tsx
@@ -0,0 +1,8 @@
+export { default as BinanceChainIcon } from './Icons/BinanceChain';
+export { default as BinanceIcon } from './Icons/Binance';
+export { default as BscScanIcon } from './Icons/BscScan';
+export { default as Coin98Icon } from './Icons/Coin98';
+export { default as CoinbaseWalletIcon } from './Icons/CoinbaseWallet';
+export { default as TrustWalletIcon } from './Icons/TrustWallet';
+export { default as WalletConnectIcon } from './Icons/WalletConnect';
+export { default as WalletIcon } from './Icons/Wallet';
